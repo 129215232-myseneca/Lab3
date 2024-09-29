fb21e9001b04ed8b9dc5640328458aa2c7544de0
Vatanveer Singh <vsingh227@myseneca.ca>
2024-09-29 18:13:10 -0400

    test1.py updated

--- test1.py
+++ test1.py
@@ -5,3 +5,10 @@ def hello():
 hello()
 hello()
 hello()
+def return_text_value():
+    name = 'Terry'
+    greeting = 'Good Morning ' + name
+    return greeting
+
+text = return_text_value()
+print(text)
