b905195ea9f3d9adf13be34b63b7d5ea0ad878e2
Vatanveer Singh <vvssandhu13@gmail.com>
2025-05-28 12:38:24 -0400

    Update lab3c.py

--- lab3c.py
+++ lab3c.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 '''Lab 3 Inv 2 function operate '''
-# Author ID: vsingh227_seneca
+# Author ID: asbegh
 
 def operate(number1, number2, operator):
     if operator == "add":
